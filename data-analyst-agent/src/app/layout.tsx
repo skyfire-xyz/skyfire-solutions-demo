65a90746fe3a103e4a57c0497bfab44b66b8a531
kmiyauchi <koji@supermojo.com>
2025-07-18 16:20:58 -0400

    Add GTag

--- data-analyst-agent/src/app/layout.tsx
+++ data-analyst-agent/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import "./globals.css";
 import { Providers } from "./providers";
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Toaster richColors />
+        <GoogleAnalytics gaId="G-YCMQRYB02G" />
       </body>
     </html>
   );
